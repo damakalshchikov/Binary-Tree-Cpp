9854d56a5c9ef6b1e1186a540c000d18c91c3e28
Mikhail Damakalshchikov <damakalshchikov@gmail.com>
2025-04-02 03:21:43 +0300

    Определены методы класса Node

--- Source Files/Node.cpp
+++ Source Files/Node.cpp
@@ -1,3 +1,29 @@
 //
 // Created by damak on 02.04.2025.
 //
+#include <string>
+#include "../Header Files/Node.h"
+
+using namespace std;
+
+/**
+ * Конструктор класса Node
+ * @param number - Числовое значение вершины
+ */
+Node::Node(int number) : number(number), left(nullptr), right(nullptr) {}
+
+/**
+ * Получить строковое представление вершины
+ * @return Строка с номером вершины
+ */
+string Node::toString() const {
+    return to_string(number);
+}
+
+/**
+ * Получить числовое значение вершины
+ * @return Номер вершины
+ */
+int Node::getNumber() const {
+    return number;
+}
