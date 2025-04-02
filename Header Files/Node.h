375920a422fe1b3292307b2650f8010fb8e440c9
Mikhail Damakalshchikov <damakalshchikov@gmail.com>
2025-04-02 03:21:00 +0300

    Определён заголовочный файл класса Node

--- Header Files/Node.h
+++ Header Files/Node.h
@@ -5,4 +5,40 @@
 #ifndef NODE_H
 #define NODE_H
 
+#include <string>
+
+using namespace std;
+
+/**
+ * Класс, представляющий вершину бинарного дерева
+ */
+class Node {
+public:
+    /**
+     * Конструктор класса Node
+     * @param number - Числовое значение вершины
+     */
+    Node(int number);
+
+    /**
+     * Получить строковое представление вершины
+     * @return Строка с номером вершины
+     */
+    string toString() const;
+
+    /**
+     * Получить числовое значение вершины
+     * @return Номер вершины
+     */
+    int getNumber() const;
+
+    // Левый и правый потомки
+    Node* left;
+    Node* right;
+
+private:
+    // Номер вершины
+    int number;
+};
+
 #endif //NODE_H
